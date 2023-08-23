535b50cd66c9b83552561b327cb8d12aec1d5ef1
Alec Odell <alecodell@Alecs-MacBook-Air.local>
2023-08-23 14:46:49 -0700

    Add basic navigation structure

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import "./AboutMe.css"
+
+function AboutMe(){
+    return(
+        <div className="AboutMe">
+            <h1 className="AboutMe-header">
+                I Am Alec
+            </h1>
+        </div>
+
+    );
+}
+
+export default AboutMe;
