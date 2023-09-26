0ca11339053e41d680de86f88aa06ffe217d248e
Alec-0dell <alec_odell@outlook.com>
2023-09-25 21:03:53 -0700

    add mobile compatability and fix some spelling on home page

--- src/Components/Home/DesktopHome.js
+++ src/Components/Home/DesktopHome.js
@@ -2,7 +2,7 @@ import './Home.css'
 import me from './Odell-2061.jpeg'
 import React, { useState, useEffect } from 'react'
 
-function Home() {
+function DesktopHome() {
 
   const [screenSize, setScreenSize] = useState(getCurrentDimension());
 
@@ -29,13 +29,13 @@ function Home() {
       <div className="Home">
         <div className='Intro-Body'>
           <header className="Home-header"> Hello, I am <br/>Alec Odell</header>
-          <p className='Intro-Para'>I am a current student at California Polytechnic Univerisity in San Luis Obispo studing to graducate in 2027 with a bachelors and masters degree in computer science. I created this website using JavaScript, HTML, CSS, and React</p>
+          <p className='Intro-Para'>I am a current student at California Polytechnic Univerisity in San Luis Obispo studying to graduate in 2027 with a bachelors and masters degree in computer science. I created this website using JavaScript, HTML, CSS, and React</p>
         </div>
         <div className='pic-container'>
-          <img src={me} alt='Picture of Me' style={{width: `${(screenSize.width/2)}px`}}/>   
+          <img src={me} alt='Alec Odell' style={{width: `${(screenSize.width/2)}px`}}/>   
         </div>
       </div>
     );
   }
   
-  export default Home;
+  export default DesktopHome;
--- src/Components/Layout/Sidebar.js
+++ src/Components/Layout/Sidebar.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import './Sidebar.css';
+import { Link } from "react-router-dom";
 
 export default props => {
   return (
     <Menu right>
-      <a className="menu-item" href="/">
-        Home
-      </a>
-      <a className="menu-item" href="/About">
-        About
-      </a>
-      <a className="menu-item" href="/Resume">
-        Resume
-      </a>
-      <a className="menu-item" href="/CodingProjects">
-        Coding Projects
-      </a>
+      <Link to="/" className="menu-item"> Home </Link>
+      <Link to="/About" className="menu-item"> About </Link>
+      <Link to="/Resume" className="menu-item"> Resume </Link>
+      <Link to="/CodingProjects" className="menu-item"> Coding Projects </Link>
       <hr>
       </hr>
       <a className="menu-item" href="https://github.com/Alec-0dell">
