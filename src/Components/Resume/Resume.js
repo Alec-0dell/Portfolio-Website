7f6d136b390dd64eb85ba0da3d85cc522c2b2865
Alec-0dell <alec_odell@outlook.com>
2023-11-12 21:55:56 -0800

    Adds new resume and download button

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,15 +1,29 @@
+import React from "react";
 import "./Resume.css"
-import ResumeImg from "./Resume3.png"
+import ResumeImg from "./ResumeDownload.png"
+
 
 function Resume(){
+    const downloadFile = (url) => {
+        const fileName = "AlecOdellResume.pdf"
+        const aTag = document.createElement("a")
+        aTag.href = url
+        aTag.setAttribute("download", fileName)
+        document.body.appendChild(aTag)
+        aTag.click()
+        aTag.remove()
+    }
+
+
     return(
         <div className="Resume">
             <header className="Resume-header">
                 My Resume
             </header>
-            <p className="Resume-Para">See a preview of my resume below and a link to the most up to date version of my resume:</p>
             <hr className="Resume-hr"></hr>
-            <a className="Resume-Button" href= "https://docs.google.com/document/d/1kAh-jHbJfAML0TQ8X9UeDCTkSDaN5GiY/edit?usp=sharing&ouid=108877786211028580908&rtpof=true&sd=true">Full Resume</a>
+            <button onClick={() => {
+                downloadFile("http://localhost:3000/Portfolio-Website/AlecOdellResume2023.pdf")
+            }} className="Resume-Button" href="React AlecOdellResume2023.pdf" download="AlecOdellResume2023.pdf">Download Resume</button>
             <hr className="Resume-hr"></hr>
             <img className="Resume-IMG" src={ResumeImg} alt="Alec Odell's Resume"></img>
         </div>
